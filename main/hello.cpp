b8c76135473db37593dd1135876f11bba80ec43b
Advaith2805 <advaithbijuraj@gmail.com>
2025-03-12 10:37:33 +0530

    cpp change

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"My name is Advaith B \n";
+  cout<<"My name is Advaith  \n";
   return 0;
 }
